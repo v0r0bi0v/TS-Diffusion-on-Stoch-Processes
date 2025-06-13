38935bdd43343d405631aab38cc3870a7b7850eb
vanyavorobiov <s19b1_vorobiov@179.ru>
2025-06-13 06:47:00 +0300

    initial commit

---
+++ report/compile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# name of the file
+FILENAME="slides"
+OUTPUT_DIR="pdf"
+
+# compile once
+pdflatex -interaction=nonstopmode -output-directory $OUTPUT_DIR $FILENAME.tex
+# biber $FILENAME
+
+# compile twice for correct bibliography
+pdflatex -interaction=nonstopmode -output-directory $OUTPUT_DIR $FILENAME.tex
+# pdflatex -interaction=nonstopmode -output-directory $OUTPUT_DIR $FILENAME.tex
+
+# delete log files
+find "$OUTPUT_DIR" -type f -name "$FILENAME.*" ! -name "$FILENAME.pdf" -delete
+
+# open
+open $OUTPUT_DIR/$FILENAME.pdf
+# xdg-open $OUTPUT_DIR/$FILENAME.pdf
+
+# git
+# git add -A
+# git commit -m "AUTO-COMMIT $(date +"%Y-%m-%d %H:%M:%S %Z %z")"
+# git push
+# if [[ $? -eq 0 ]]; then
+#     echo "Pushed successfully"
+# else
+#     echo "Not pushed"
+# fi
